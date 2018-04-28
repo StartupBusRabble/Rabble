40d51dd7c1be372decb0985f228c4010b4b2839d
John Malloy <soccerboymalloy@gmail.com>
2018-04-27 23:15:45 -0400

    Initial Questionnaire page setup

--- rabble/config/routes.rb
+++ rabble/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "application#index"
+  get "/:page" => "application#show"
 
   devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 end
