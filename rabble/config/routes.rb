656b42f10dc85452018ca4f55037a828d4f1ff3a
Jack Huang <jack7100@gmail.com>
2018-04-28 07:43:00 -0400

    Add bootstrap 4, add questionnaire controller and view

--- rabble/config/routes.rb
+++ rabble/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "application#index"
-  get "/:page" => "application#show"
   devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 
   resources :questionnaire, only: [:index, :show]
