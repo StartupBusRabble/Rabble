e7e9403df855e30b29265e2ecc0ceb8260e6c86e
Jack Huang <jack7100@gmail.com>
2018-04-30 01:40:54 -0500

    Redirect route to group page after user has filled out questionnaire

--- rabble/app/controllers/application_controller.rb
+++ rabble/app/controllers/application_controller.rb
@@ -4,13 +4,10 @@ class ApplicationController < ActionController::Base
   def index
     if (current_user && !current_user.is_user_ready_for_match)
       redirect_to questionnaire_index_url
+    elsif current_user && current_user.is_user_ready_for_match
+      redirect_to group_index_url
     else
       render "index.html"
     end
-  end
-
-  private
-  def valid_page?
-    File.exist?(Pathname.new(Rails.root + "app/views/application/#{params[:page]}.html.erb"))
   end
 end
