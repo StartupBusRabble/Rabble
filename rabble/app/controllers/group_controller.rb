eb06ebc8665bda0270b4ff7e57a052ec5a57e41a
John Malloy <soccerboymalloy@gmail.com>
2018-04-30 04:43:30 -0400

    Add default event

--- rabble/app/controllers/group_controller.rb
+++ rabble/app/controllers/group_controller.rb
@@ -4,7 +4,7 @@ class GroupController < ApplicationController
   def index
     if current_user && current_user.groups_id?
       @chatName = Group.find(current_user.groups_id).chat_name
-      @event = Event.find_by_id(Group.find(current_user.groups_id).id)
+      @event = Event.where(groups_id: Group.find(current_user.groups_id).id).take
       @matchedUsers = User.where(groups_id: current_user.groups_id).where.not(id: current_user.id)
     end
     render "group.html"
--- rabble/app/helpers/matchmaker.rb
+++ rabble/app/helpers/matchmaker.rb
@@ -5,6 +5,8 @@ class Matchmaker < ApplicationController
     if matched_joined_users.count == 3
       chat_channel = generate_unique_chat_channel_name()
       new_group = Group.create(chat_name: chat_channel)
+      Event.create(venue: "After party", address: "The place", date: "Monday night", groups_id: new_group.id)
+
       current_user.groups_id = new_group.id
       current_user.matched = true
       matched_joined_users.each do |matched_joined_user|
